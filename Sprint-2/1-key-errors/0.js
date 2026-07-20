c06800222b6cf5e16949d1654818c01937dad16c
Abid Akhtar <Abidakhtar2023@gmail.com>
2026-07-21 00:02:46 +0100

    Fix error in 0.js file

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,6 +1,8 @@
 // Predict and explain first...
 //  =============> write your prediction here
 
+// I think the code will not run and JS will throw an error because the variable Str is declared twice
+
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
@@ -10,4 +12,13 @@ function capitalise(str) {
 }
 
 // =============> write your explanation here
-// =============> write your new code here
+
+// I will use a different variable name and then run the code to see if it works 
+
+
+// write your new code here
+
+function capitalise(str) {
+  let capitalised = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return capitalised;
+}
